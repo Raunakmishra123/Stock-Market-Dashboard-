bb966d33f9977f0fcdef66be845e62bb32090521
raunakmishra123 <raunakmishramth20@gmail.com>
2025-08-11 18:58:10 +0530

    Created navigation bar UI

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+export default function Navbar() {
+  return (
+    <nav className="p-4 bg-slate-900 border-b border-slate-800 text-white w-full flex justify-between">
+      <h1 className="text-xl font-bold">StockPulse</h1>
+    </nav>
+  );
+}
